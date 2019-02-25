122868200d0c19998a7b35849d1859b28b093104
Johny_dudek <johny.dudek@gmail.com>
2019-02-25 20:47:54 +0100

    User registration

--- garmax/app/controllers/users_controller.rb
+++ garmax/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Successfully added account"
+      redirect_to @user
+    else
+      flash[:error] = "Error adding account"
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
